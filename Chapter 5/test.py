80b29c2d15322c444ad65387e7d3428ad5181d5c
sangyeon-park <tkddus0421@gmail.com>
2023-03-28 22:25:44 +0900

    study_2023.03.28

--- Chapter 5/test.py
+++ Chapter 5/test.py
@@ -1,5 +1,3 @@
-import numpy as np 
-import sys 
 import torch
 from torch import nn
 from torch import optim
@@ -7,19 +5,28 @@ import numpy as np
 from torch.nn import functional as F
 import gym
 import torch.multiprocessing as mp #A
-import os
 
-def square2(i, x, queue):
-    print("In process {}".format(i,))
-    queue.put(np.square(x))
-    sys.stdout.flush()
-    
+class ActorCritic(nn.Module): #B
+    def __init__(self):
+        super(ActorCritic, self).__init__()
+        self.l1 = nn.Linear(4,25)
+        self.l2 = nn.Linear(25,50)
+        self.actor_lin1 = nn.Linear(50,2)
+        self.l3 = nn.Linear(50,25)
+        self.critic_lin1 = nn.Linear(25,1)
 
-def square(x):
-    return x*x
-
-def shit(k):
-    return k*2
+    def forward(self,x):
+        x = F.normalize(x,dim=0)
+        y = F.relu(self.l1(x))
+        y = F.relu(self.l2(y))
+        # l2를 통과한 결과로 actor와 critic을 모두 사용한다 
+        # log_softmax(x) := log(softmax(x)) 와 같지만 따로 사용하는 것보다 안정성이 높음 
+        actor = F.log_softmax(self.actor_lin1(y),dim=0) #C
+        c = F.relu(self.l3(y.detach()))  # critic은 역전파되지 않음 
+        # critic은 -1~1 사이의 값을 출력한다 
+        # 이러한 값은 보상이 -1, +1인 cartpole과 잘 맞는다 
+        critic = torch.tanh(self.critic_lin1(c)) #D
+        return actor, critic #E
 
 ##### Listing 5.8
 def update_params(worker_opt,values,logprobs,rewards,clc=0.1,gamma=0.95):
@@ -63,7 +70,7 @@ def run_episode(worker_env, worker_model):
         logprob_ = policy.view(-1)[action]
         logprobs.append(logprob_)
         # 뽑은 action에 따라 다음 step 진행 
-        state_, _, done, info = worker_env.step(action.detach().numpy())
+        state_, _, done, __, info = worker_env.step(action.detach().numpy())
         state = torch.from_numpy(state_).float()
         if done: #F
             reward = -10
@@ -73,46 +80,45 @@ def run_episode(worker_env, worker_model):
         rewards.append(reward)
     return values, logprobs, rewards
 
-def info(title):
-    print (title)
-    print ('module name:', __name__)
-    print ('parent process:', os.getppid())
-    print ('process id:', os.getpid())
-
-class ActorCritic(nn.Module):
-    def __init__(self):
-        super(ActorCritic, self).__init__()
-        self.l1 = nn.Linear(4,25)
-        self.l2 = nn.Linear(25,50)
-        self.actor_lin1 = nn.Linear(50,2)
-        self.l3 = nn.Linear(50,25)
-        self.critic_lin1 = nn.Linear(25,1)
-    def forward(self,x):
-        x = F.normalize(x,dim=0)
-        y = F.relu(self.l1(x))
-        y = F.relu(self.l2(y))
-        actor = F.log_softmax(self.actor_lin1(y),dim=0)
-        c = F.relu(self.l3(y.detach()))
-        critic = torch.tanh(self.critic_lin1(c))
-        return actor, critic
 # Listing 5.6
-def cworker(t, worker_model, counter, params):
+def worker(t, worker_model, counter, params):
     # print("t is:",t, flush=True)
-    sys.stdout = open(str(os.getpid()) + ".out", "w")
-    info('function cworker')
-    print ('hello')
+    # sys.stdout = open(str(os.getpid()) + ".out", "w")
+    # info('function cworker')
+    # print ('hello')
     worker_env = gym.make("CartPole-v1")
     worker_env.reset()
     # 하나의 모형을 모든 프로세스가 공유 
     worker_opt = optim.Adam(lr=1e-4,params=worker_model.parameters()) #A
     worker_opt.zero_grad()
     for i in range(params['epochs']):
+        if i%100==0: print(i)
         worker_opt.zero_grad()
         # episode를 진행하여 데이터를 수집하고 매개변수들을 갱신한다 
         values, logprobs, rewards = run_episode(worker_env,worker_model) #B 
         actor_loss,critic_loss,eplen = update_params(worker_opt,values,logprobs,rewards) #C
         counter.value = counter.value + 1 #D
 
+MasterNode = ActorCritic()
+MasterNode.share_memory()  # 프로세스들이 model의 매개변수를 복사하는게 아니라 공유하게 함 
+processes = []
+params = {
+    'epochs':1000,
+    'n_workers':7,
+}
+# 내장 공유 객체를 전역 공유 카운터로 사용
+counter = mp.Value('i',0)  # i는 정수라는 뜻 
+if __name__ == '__main__': #adding this for process safety
+    for i in range(params['n_workers']):
+        p = mp.Process(target=worker, args=(i,MasterNode,counter, params))
+        p.start()
+        processes.append(p)
 
+    print(len(processes))
+    for p in processes:
+        p.join()
 
-
+    for p in processes:
+        p.terminate()
+    # 전역 카운터의 값과 첫 프로세스의 종료코드 출력(문제 없으면 0)
+    print(counter.value,processes[1].exitcode)
